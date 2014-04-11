50a1254a457b2a67144ce8e8eed78f3e0d6bbd1a
Marko Raatikka <make@kapsi.fi>
2014-04-12 00:49:19 +0300

    Fixed gulp-useref

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,70 +1,85 @@
 var gulp = require('gulp'),
-   livereloadserver = require('tiny-lr')(),
-	 browserify = require('browserify'),
-	 exec = require('exec'),
-   open = require('open'),
-	 ripple = require('ripple-emulator'),
-   chalk = require('chalk'),
-   runSequence = require('run-sequence'),
-   config = require('./gulpfile.config.js'),
-   $ = require('gulp-load-plugins')();
+    livereloadserver = require('tiny-lr')(),
+    browserify = require('browserify'),
+    exec = require('exec'),
+    open = require('open'),
+    ripple = require('ripple-emulator'),
+    chalk = require('chalk'),
+    wiredep = require('wiredep').stream,
+    runSequence = require('run-sequence'),
+    config = require('./gulpfile.config.js'),
+    $ = require('gulp-load-plugins')();
 
 gulp.task('lint', function () {
-    return gulp.src(config.wwwPath('js/**.js'))
+    return gulp.src(config.wwwPath('scripts/**.js'))
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('scripts', ['lint'], function () {
-  return gulp.src(config.wwwPath('js/**/*.js'))
-      .pipe($.jscs())
-      .pipe(gulp.dest(config.destPath('js')))
-      .pipe($.size());
+    return gulp.src(config.wwwPath('scripts/**/*.js'))
+        .pipe($.jscs())
+        .pipe($.size());
 });
 
 gulp.task('styles', function () {
-  return gulp.src(config.wwwPath('css/**/*.css'))
-      .pipe($.autoprefixer('last 1 version'))
-      .pipe(gulp.dest(config.destPath('css')))
-      .pipe($.size());
+    return gulp.src(config.wwwPath('styles/**/*.scss'))
+        .pipe($.rubySass())
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest(config.wwwPath('styles')))
+        .pipe($.size());
 });
 
 gulp.task('images', function () {
-  return gulp.src(config.wwwPath('img/**/*.{jpg,jpeg,gif,png}'))
-      .pipe($.cond(config.production, $.imagemin()))
-      .pipe(gulp.dest(config.destPath('img')))
-      .pipe($.size());
+    return gulp.src(config.wwwPath('images/**/*'))
+        .pipe($.cond(config.production, $.imagemin()))
+        .pipe(gulp.dest(config.destPath('images')))
+        .pipe($.size());
 });
 
-gulp.task('html', function () {
-  return gulp.src(config.wwwPath('*.html'))
-      .pipe($.cond(!config.production, $.embedlr()))
-      .pipe(gulp.dest(config.destPath()))
-      .pipe($.size());
+gulp.task('html', ['styles', 'scripts'], function () {
+    return gulp.src(config.wwwPath('*.html'))
+        .pipe($.cond(!config.production, $.embedlr()))
+        .pipe($.useref.assets())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest(config.destPath()))
+        .pipe($.size());
+});
+
+gulp.task('wiredep', function () {
+
+    return gulp.src(config.wwwPath('*.html'))
+        .pipe(wiredep({
+            directory: config.vendorPath,
+            ignorePath: config.wwwPath()
+        }))
+        .pipe(gulp.dest(config.wwwPath()));
+});
+
+
+gulp.task('prepare', function (cb) {
+    exec(['cordova', 'prepare'], function (err, out, code) {
+        if (err instanceof Error) {
+          throw err;
+        }
+        cb();
+    });
+});
+
+gulp.task('connect', function () {
+    ripple.emulate.start({port: 4000});
+    livereloadserver.listen(config.livereloadport);
+});
+
+gulp.task('serve', ['connect'], function () {
+    var url = 'http://localhost:' + config.ripple.port + '/index.html' + config.ripple.queryString;
+    $.util.log('Running Ripple Emulator at ' + chalk.cyan(url));
+    open(url);
 });
 
 gulp.task('clean', function () {
-  return gulp.src(config.destPath(), { read: false }).pipe($.clean());
-});
-
-gulp.task('prepare', function (cb) {
-  exec(['cordova', 'prepare'], function (err, out, code) {
-      if (err instanceof Error) {
-        throw err;
-      }
-      cb();
-  });
-});
-
-gulp.task('serve', function () {
-    var url = 'http://localhost:' + config.ripple.port + '/index.html' + config.ripple.queryString;
-
-    ripple.emulate.start({port: 4000});
-    livereloadserver.listen(config.livereloadport);
-
-    $.util.log('Running Ripple Emulator at ' + chalk.cyan(url));
-
-    open(url);
+    return gulp.src(config.destPath(), { read: false }).pipe($.clean());
 });
 
 gulp.task('watch', ['serve'], function () {
@@ -75,16 +90,15 @@ gulp.task('watch', ['serve'], function () {
       });
     });
 
-    gulp.watch(config.wwwPath('css/**/*.css'), ['styles']);
-    gulp.watch(config.wwwPath('js/**/*.js'), ['scripts']);
+    gulp.watch(config.wwwPath('styles/**/*.css'), ['styles']);
+    gulp.watch(config.wwwPath('scripts/**/*.js'), ['scripts']);
+    gulp.watch(config.wwwPath('images/**/*'), ['images']);
     gulp.watch(config.wwwPath('*.html'), ['html']);
-    gulp.watch(config.wwwPath('img/**/*.{jpg,jpeg,gif,png}'), ['images']);
+    gulp.watch('bower.json', ['wiredep']);
 });
 
-gulp.task('default', function () {
-  return runSequence(['build'], 'watch');
-});
+gulp.task('build', ['html', 'images']);
 
-gulp.task('build', function (cb) {
-  return runSequence('clean', ['scripts', 'styles', 'images', 'html'], 'prepare', cb);
+gulp.task('default', ['clean'], function () {
+  return gulp.start('build');
 });
